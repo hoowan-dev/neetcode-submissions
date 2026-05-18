2707a0a453e7c058fdc0d279fcbff5db321f066b
Juan Becerra <becerrajuan007@gmail.com>
2026-05-18 14:44:57 -0700

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        permutations = []
+
+        def permuteRec(currPermutation, used):
+            if len(currPermutation) == len(nums):
+                permutations.append(currPermutation.copy())
+                return
+
+            for i, n in enumerate(used):
+                if used[i] == False:
+                    currPermutation.append(nums[i])
+                    used[i] = True
+
+                    permuteRec(currPermutation, used)
+
+                    currPermutation.pop()
+                    used[i] = False
+
+        permuteRec([], [False] * len(nums))
+        return permutations
